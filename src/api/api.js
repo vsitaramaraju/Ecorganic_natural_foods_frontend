594907b724a246c07272018414e389918166330b
unknown <ajithbala50@gmail.com>
2026-08-07 18:32:41 +0530

    ip url

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "http://200.141.1.171:5000/api";
 
 const getToken = () => localStorage.getItem("token");
 
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "http://200.141.1.171:5000/api"
 });
 
 API.interceptors.request.use(config => {

9e2d9d5cd621df6cc20ea476235f4640d898a93e
unknown <ajithbala50@gmail.com>
2026-09-08 17:50:37 +0530

    globalURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: API_BASE_URL
 });
 
 API.interceptors.request.use(config => {
